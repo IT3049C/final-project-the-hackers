af21ef51a71c342914237ab5752063cae330e62e
Jonathan Douglas <jddoug2005@gmail.com>
2026-04-20 16:52:04 -0400

    fixed wordle game to show up on main site

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import './App.css'
+import "./App.css";
+import WordleGame from "./WordleGame.jsx"; // Import the file you renamed in Step 1
 
 function App() {
-
   return (
     <>
       <h1>GameHub</h1>
       <p>Welcome to GameHub</p>
+
+      {/* Display the Wordle Game here */}
+      <WordleGame />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
